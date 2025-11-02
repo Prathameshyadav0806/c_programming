0a38171f4eb95b2ab42450ea7106786cb03209b7
Prathameshyadav0806 <prathameshyadav400@gmail.com>
2025-11-02 13:54:23 +0530

    Cheack year is leap year or not.

---
+++ print_year_is_leap_year_or_not_ass_40.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * Main enttry of code.
+ * @return : Returns 0 after sucessfull execution of code
+ */
+int main(){
+    int year;
+    printf("Enter year :");
+    scanf("%d", &year);
+
+    if(year % 4 == 0) {
+        printf("%d is leap year.", year);
+    }
+    else {
+        printf("%d is not leap year.", year);
+    }
+
+    return 0;
+}
